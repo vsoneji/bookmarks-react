6afa9afb92d9d94006df20cf2556b74504f960b7
vsoneji <vsoneji@hotmail.com>
2022-04-21 19:36:31 -0400

    use different json editor

--- src/components/BookmarksEditor.tsx
+++ src/components/BookmarksEditor.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import { JsonEditor as Editor } from 'jsoneditor-react';
-import 'jsoneditor-react/es/editor.min.css';
 import { sampleData } from '../model/sampleData';
 import styled from 'styled-components';
+import JSONInput from 'react-json-editor-ajrm';
+
+// @ts-ignore
+import locale from 'react-json-editor-ajrm/locale/en';
 
 const BigEditor = styled.div`
-    height: 800px;
+
 `;
 
 export const BookmarksEditor: React.FunctionComponent = () => {
+
     return (
         <BigEditor>
-            <Editor 
-                value={sampleData} 
-                mode="code" 
-                mainMenuBar={false}
-            />
+            <JSONInput
+                    id          = 'a_unique_id'
+                    height      = '500px'
+                    width       = '800px'
+                    theme       ='dark_vscode_tribute'
+                    locale      = {locale}
+                    placeholder = {sampleData}
+                />
         </BigEditor>
     )
 }
